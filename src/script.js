0b4313f3407283f16fed1857ccb7784fc2f15dae
thomasryu <thomas.sameshima@gmail.com>
2022-12-07 20:51:58 -0300

    Attempting zoom listener

--- src/script.js
+++ src/script.js
@@ -103,6 +103,20 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+window.visualViewport.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
 // Base camera
 let cameraDistance = 0;
 const setCameraDistance = () => {
